4be7ed9a515283a8064fd83a7841ae6580723c45
Mustafa Dhar <62246143+msquare-2@users.noreply.github.com>
2022-01-04 23:43:01 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (70.99%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i = 0; i < n; i++) {
-            nums1[m+i] = nums2[i];
+        for(int i = m; i < nums1.size(); i++) nums1[i]=nums2[i-m];
+            int gap = (n+m)%2?(n+m):(n+m+1);
+        
+        // Using Gap algorithem for sorting
+        while(gap > 1){
+            gap = gap%2==0?gap/2:gap/2+1;
+            for(int i = 0 ; i < nums1.size(); i++){
+                if((i+gap < nums1.size()) && nums1[i] > nums1[i+gap]) swap(nums1[i], nums1[i+gap]);
+            }
+            
         }
-        sort(nums1.begin(), nums1.end());
     }
 };
