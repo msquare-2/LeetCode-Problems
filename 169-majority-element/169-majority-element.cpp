fc55904bd0ad1982af7d3dacd22d53905f69fbb6
Mustafa Dhar <62246143+msquare-2@users.noreply.github.com>
2022-03-08 19:08:56 +0530

    Time: 23 ms (66.71%), Space: 19.6 MB (51.93%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        //Moore Voting Algo
-        int n = nums.size();
         int elem = 0;
         int count = 0;
         for(auto it : nums) {
             if(count == 0)
                 elem = it;
             if(it == elem)
-                count += 1;
-            else
-                count -= 1;
+                count++;
+            if(it != elem)
+                count--;
         }
         return elem;
-        
     }
 };
