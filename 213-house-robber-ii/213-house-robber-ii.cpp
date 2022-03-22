0b077f5224bbeec069d07c39c0a154064a66223d
Mustafa Dhar <62246143+msquare-2@users.noreply.github.com>
2022-03-22 20:42:42 +0530

    Time: 3 ms (55.55%), Space: 7.9 MB (40.30%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int maxmoney(vector<int> &nums){
+        int n = nums.size();
+        int prev1 = nums[0];
+        int prev2 = 0;
+        for(int i=0; i<n; i++){
+            int take = nums[i];
+            if(i-2 >= 0)
+                take += prev2;
+            int notake = 0;
+            if(i-1 >= 0)
+                notake += prev1;
+            int cur = max(take, notake);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1)
+            return nums[0];
+        vector<int> temp1, temp2;
+        for(int i=0; i<n; i++){
+            if(i != 0)
+                temp1.push_back(nums[i]);
+            if(i != n-1)
+                temp2.push_back(nums[i]);
+        }
+        return max(maxmoney(temp1), maxmoney(temp2));
+    }
+};
